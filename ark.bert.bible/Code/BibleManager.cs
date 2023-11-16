0ee07f2a0db24e2a0f42e3a7aea9a5491ca21b8f
Immanuel Raj <kimr@solar.dk>
2023-11-16 10:42:48 +0530

    changes.

--- ark.bert.bible/Code/BibleManager.cs
+++ ark.bert.bible/Code/BibleManager.cs
@@ -18,6 +18,76 @@
         //static Func<string> select_books_kjv = () => @$"SELECT b, n as book from key_english";
         static Func<string> select_books_kjv = () => @$"SELECT t.b, ke.n as book, t.c, t.v, t.id, t.t from key_english ke, t_kjv t where t.b = ke.b";
     }
+    public class RecordManger
+    {
+        public static dynamic GetLogs(string tbl)
+        {
+            return new Ark.Sqlite.SqliteManager(con_str).Select($"select * from {tbl}");
+        }
+        public static List<string> GetMonthlyTable()
+        {
+            string qry = @"SELECT 
+                            name
+                        FROM 
+                            sqlite_schema
+                        WHERE 
+                            type ='table' AND 
+                            name NOT LIKE 'sqlite_%';";
+            return new Ark.Sqlite.SqliteManager(con_str).ExecuteSelect<string>(qry).ToList();
+        }
+        static Func<string> log_table_name = () => $"logs_{DateTime.Now.Year}_{DateTime.Now.Month.ToString().PadLeft(2, '0')}"; // Yeear & MOnth level file
+        public static void Log(Question q)
+        {
+            var tbl = log_table_name();
+            new Ark.Sqlite.SqliteManager(con_str).CreateTable(create_table_logs(tbl));
+            new Ark.Sqlite.SqliteManager(con_str).ExecuteQuery(insert_log_record(tbl, q));
+
+        }
+        static string con_str = $"Data Source=./Models/log-sqlite.db";
+        static Func<string?, string> cleanup = str => Ark.Sqlite.SqliteManager.ReplaceSpecialChar(str ?? "", new Dictionary<string, string?>() { { "'", "''" } });
+        static Func<string, string> create_table_logs = (tbl_name) =>
+        @$"CREATE TABLE  IF NOT EXISTS {tbl_name} (
+    log_id INTEGER NOT NULL
+                      CONSTRAINT PK_logs_id PRIMARY KEY AUTOINCREMENT,
+    books     TEXT,
+    context   TEXT,
+    ids       TEXT,
+    verses    TEXT,
+    chapters  TEXT,
+    question  TEXT,
+    answers   TEXT,
+    confidence   TEXT,
+    ip        TEXT,
+    at        TEXT
+);
+";
+        static Func<string, Question, string> insert_log_record = (tbl_name, question) =>
+        @$"INSERT INTO {tbl_name} (
+                        books,
+                        context,
+                        ids,
+                        verses,
+                        chapters,
+                        question,
+                        answers,
+                        confidence,
+                        ip,
+                        at
+                    )
+                    VALUES (
+                        '{cleanup(Newtonsoft.Json.JsonConvert.SerializeObject(question.books))}',
+                        '{cleanup(question.context)}',
+                        '{cleanup(Newtonsoft.Json.JsonConvert.SerializeObject(question.ids))}',
+                        '{cleanup(Newtonsoft.Json.JsonConvert.SerializeObject(question.verses))}',
+                        '{cleanup(Newtonsoft.Json.JsonConvert.SerializeObject(question.chapters))}',
+                        '{cleanup(Newtonsoft.Json.JsonConvert.SerializeObject(question.question))}',
+                        '{cleanup(Newtonsoft.Json.JsonConvert.SerializeObject(question.answers))}',
+                        '{cleanup(Newtonsoft.Json.JsonConvert.SerializeObject(question.confidence))}',
+                        '{question.ip}',
+                        '{DateTime.UtcNow}'
+                    );
+";
+    }
     public class Book
     {
         public int b { get; set; } //book index
@@ -30,6 +100,14 @@
     public class Question
     {
         public string context { get; set; }
+        public long[] ids { get; set; }
+        public int[] verses { get; set; }
+        public int[] chapters { get; set; }
+        public string[] books { get; set; }
         public string[] question { get; set; }
+        public string[] answers { get; set; }
+        public float[] confidence { get; set; }
+        public string ip { get; set; }
+        public string at { get; set; }
     }
 }
--- ark.bert.bible/Controllers/HomeController.cs
+++ ark.bert.bible/Controllers/HomeController.cs
@@ -3,21 +3,38 @@ using Microsoft.AspNetCore.Server.IIS.Core;
 using Microsoft.ML.Models.BERT;
 using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using System.Linq;
 
 namespace ark.bert.bible.Controllers
 {
     public class HomeController : Controller
     {
         private readonly BertModel _bertModel;
-        private readonly ILogger<HomeController> _logger;
         private readonly BibleManager _bible;
-        public HomeController(ILogger<HomeController> logger, BibleManager bible, BertModel bertModel)
+        public HomeController(BibleManager bible, BertModel bertModel)
         {
-            _logger = logger;
             _bible = bible;
             _bertModel = bertModel;
         }
-
+        public IActionResult TrackLog()
+        {
+            var tbls = RecordManger.GetMonthlyTable().OrderByDescending(t => t).ToList();
+            ViewBag.Tracks = tbls;
+            var tbb = tbls.Count > 0 ? tbls[0] : "";
+            ViewBag.Tbl = tbb;
+            ViewBag.TrackLog = RecordManger.GetLogs(tbb);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult TrackLog([FromForm] string table)
+        {
+            var tbls = RecordManger.GetMonthlyTable().OrderByDescending(t => t).ToList();
+            ViewBag.Tracks = tbls;
+            var tbb = !string.IsNullOrEmpty(table) ? table : tbls.Count > 0 ? tbls[0] : "";
+            ViewBag.Tbl = tbb;
+            ViewBag.TrackLog = RecordManger.GetLogs(tbb); 
+            return View();
+        }
         public IActionResult Index()
         {
             ViewBag.Bible = BibleManager.hie;
@@ -37,17 +54,20 @@ namespace ark.bert.bible.Controllers
 
         [HttpPost]
         [Route("predict")]
-        public dynamic SubmitQuestion([FromBody]Question question)
+        public dynamic SubmitQuestion([FromBody] Question question)
         {
             var prob = new List<(string, (List<string>, float))>();
             (question.question ?? new string[] { }).ToList().ForEach(x =>
             {
                 prob.Add((x, _bertModel.Predict(question.context, x)));
             });
-
+            var ans = prob.Select(t => "Question: " + t.Item1 + "<br />" + "Answer:" + string.Join(' ', t.Item2.Item1) + "<br />" + "Confidence Level:" + t.Item2.Item2.ToString()).ToList();
+            question.answers = prob.Select(t => string.Join(' ', t.Item2.Item1)).ToArray();
+            question.confidence = prob.Select(t => t.Item2.Item2).ToArray();
+            RecordManger.Log(question);
             return new
             {
-                Results = prob.Select(t => "Question: " +  t.Item1 + "<br />" + "Answer:" + string.Join(' ',t.Item2.Item1) + "<br />" + "Confidence Level:" + t.Item2.Item2.ToString()).ToList() 
+                Results = ans
             };
         }
         public dynamic GetBible([FromQuery] string book, [FromQuery] int chapter)
